2fe5bc86692171516caac23bb1c783c8ee35ca0b
shyin <shyin@shyins-MacBook-Pro.local>
2022-09-23 15:50:21 +0800

    xmind modify

--- conftest.py
+++ conftest.py
@@ -8,13 +8,13 @@ fakerestApi = "https://fakerestapi.azurewebsites.net"
 @pytest.fixture()
 def api_v1_books ():
    api = "/api/v1/Books"
-   LOGGER.info (api + " Triggered")
+   LOGGER.info (fakerestApi + api + " Triggered")
    return fakerestApi + api
 
 
 @pytest.fixture()
 def api_v1_users ():
    api = "/api/v1/Users"
-   LOGGER.info (api + " Triggered")
+   LOGGER.info (fakerestApi + api + " Triggered")
    return fakerestApi + api
 
